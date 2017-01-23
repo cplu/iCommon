69427884ee7107a3ea491bb2a7cc577862e4dfe1
cplu <chenpinglu01@gmail.com>
2017-01-23 10:42:26 +0800

    Add files via upload

---
+++ src/androidTest/java/com/windfindtech/icommon/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.windfindtech.icommon;
+import junit.framework.TestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends TestCase {
+    public void testMain() throws Exception {
+        System.out.println("hello test");
+    }
+}
